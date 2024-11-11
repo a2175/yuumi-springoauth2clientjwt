80fda6cdd8624ac48c716aeecca75672e6355a3c
a2175 <as217512@gmail.com>
2024-11-11 17:31:51 +0900

    init

---
+++ src/main/java/hello/springoauth2clientjwt/Springoauth2clientjwtApplication.java
@@ -0,0 +1,13 @@
+package hello.springoauth2clientjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springoauth2clientjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springoauth2clientjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springoauth2clientjwt/Springoauth2clientjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springoauth2clientjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springoauth2clientjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
