5577d6d224f7b8e24dbdda3f95497f3c68584de7
a2175 <as217512@gmail.com>
2024-11-20 16:39:19 +0900

    스프링 OAuth2 클라이언트 JWT 15 : CORS 설정
    https://www.youtube.com/watch?v=Xiv5KFoEW4w&list=PLJkjrxxiBSFALedMwcqDw_BPaJ3qqbWeB&index=15

--- src/main/java/hello/springoauth2clientjwt/config/SecurityConfig.java
+++ src/main/java/hello/springoauth2clientjwt/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import hello.springoauth2clientjwt.jwt.JWTFilter;
 import hello.springoauth2clientjwt.jwt.JWTUtil;
 import hello.springoauth2clientjwt.oauth2.CustomSuccessHandler;
 import hello.springoauth2clientjwt.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +29,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         //csrf disable
         http
